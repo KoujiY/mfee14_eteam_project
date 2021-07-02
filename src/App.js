841092805718fc6f5e93b04d88c42bbdc41b9cdb
wworveaory <wworveaory@gmail.com>
2021-07-02 10:17:41 +0800

    xs
    xs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Nav from './pages/nav'
 
 import Index from './pages/index'
+
+import Footer from './pages/footer'
 
 function App() {
   return (
@@ -11,7 +14,9 @@ function App() {
       <>
         <Switch>
           <Route exact path="/">
+            <Nav />
             <Index />
+            <Footer />
           </Route>
         </Switch>
       </>
