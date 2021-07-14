3a931677ca57ef9b0517fb949e0a89c8bbd09774
wworveaory <wworveaory@gmail.com>
2021-07-14 16:38:43 +0800

    nav70%

--- src/pages/nav/nav-index.js
+++ src/pages/nav/nav-index.js
@@ -164,7 +164,7 @@ function Nav() {
                   {/*?xml version="1.0" encoding="iso-8859-1"?*/}
                   {/* Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  */}
                   <svg
-                    className="nav-icon-4 bx bxs-chevron-down arrow htmlcss-arrow"
+                    className="nav-icon-user-home-arrow  bx bxs-chevron-down arrow htmlcss-arrow"
                     version="1.1"
                     id="Capa_1"
                     xmlns="http://www.w3.org/2000/svg"
@@ -217,7 +217,7 @@ function Nav() {
                   {/*?xml version="1.0" encoding="iso-8859-1"?*/}
                   {/* Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  */}
                   <svg
-                    className="nav-icon-4 bx bxs-chevron-down arrow js-arrow"
+                    className="nav-icon-user-home-arrow bx bxs-chevron-down arrow js-arrow"
                     version="1.1"
                     id="Capa_1"
                     xmlns="http://www.w3.org/2000/svg"
@@ -282,7 +282,7 @@ function Nav() {
                   {/*?xml version="1.0" encoding="iso-8859-1"?*/}
                   {/* Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  */}
                   <svg
-                    className="nav-icon-4 bx bxs-chevron-down arrow all-arrow"
+                    className="nav-icon-user-home-arrow bx bxs-chevron-down arrow all-arrow"
                     version="1.1"
                     id="Capa_1"
                     xmlns="http://www.w3.org/2000/svg"
@@ -344,8 +344,157 @@ function Nav() {
               </ul>
             </div>
             <div className="loginbar">
-              <div>登入/註冊</div>
-              <div>店家專區</div>
+              <div className="loginbar-left">
+                <a href="#/">
+                  {/*?xml version="1.0" encoding="iso-8859-1"?*/}
+                  {/* Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  */}
+                  <svg
+                    className="nav-icon-user-home-arrow"
+                    version="1.1"
+                    id="Capa_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 512 512"
+                    style={{ enableBackground: 'new 0 0 512 512' }}
+                    xmlSpace="preserve"
+                  >
+                    <g>
+                      <g>
+                        <path
+                          d="M437.02,330.98c-27.883-27.882-61.071-48.523-97.281-61.018C378.521,243.251,404,198.548,404,148
+			C404,66.393,337.607,0,256,0S108,66.393,108,148c0,50.548,25.479,95.251,64.262,121.962
+			c-36.21,12.495-69.398,33.136-97.281,61.018C26.629,379.333,0,443.62,0,512h40c0-119.103,96.897-216,216-216s216,96.897,216,216
+			h40C512,443.62,485.371,379.333,437.02,330.98z M256,256c-59.551,0-108-48.448-108-108S196.449,40,256,40
+			c59.551,0,108,48.448,108,108S315.551,256,256,256z"
+                        />
+                      </g>
+                    </g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                  </svg>
+                  登入/註冊
+                </a>
+              </div>
+              <div className="loginbar-center">
+                <a href="#/">
+                  {/*?xml version="1.0" encoding="iso-8859-1"?*/}
+                  {/* Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  */}
+                  <svg
+                    className="nav-icon-user-home-arrow"
+                    version="1.1"
+                    id="Capa_1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 477.846 477.846"
+                    style={{ enableBackground: 'new 0 0 477.846 477.846' }}
+                    xmlSpace="preserve"
+                  >
+                    <g>
+                      <g>
+                        <path
+                          d="M472.847,226.846l-34.116-34.116L250.998,4.997c-6.664-6.663-17.468-6.663-24.132,0L39.132,192.73L4.999,226.864
+			c-6.548,6.78-6.361,17.584,0.419,24.132c6.614,6.388,17.099,6.388,23.713,0l4.983-5.018v214.801
+			c0,9.426,7.641,17.067,17.067,17.067h375.467c9.426,0,17.067-7.641,17.067-17.067V245.978l5,5.001
+			c6.78,6.548,17.584,6.36,24.132-0.419C479.235,243.946,479.235,233.46,472.847,226.846z M290.115,443.713h-102.4V307.179h102.4
+			V443.713z M409.581,443.713h-85.333v-153.6c0-9.426-7.641-17.067-17.067-17.067H170.648c-9.426,0-17.067,7.641-17.067,17.067
+			v153.6H68.248V211.845L238.914,41.178l170.667,170.667V443.713z"
+                        />
+                      </g>
+                    </g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                    <g></g>
+                  </svg>
+                  店家專區
+                </a>
+              </div>
+              <div className="loginbar-right">
+                {/*?xml version="1.0" encoding="iso-8859-1"?*/}
+                {/* Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  */}
+                <svg
+                  className="nav-icon-chart"
+                  version="1.1"
+                  id="Capa_1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 450.391 450.391"
+                  style={{ enableBackground: 'new 0 0 450.391 450.391' }}
+                  xmlSpace="preserve"
+                >
+                  <g>
+                    <g>
+                      <g>
+                        <path
+                          d="M143.673,350.322c-25.969,0-47.02,21.052-47.02,47.02c0,25.969,21.052,47.02,47.02,47.02
+				c25.969,0,47.02-21.052,47.02-47.02C190.694,371.374,169.642,350.322,143.673,350.322z M143.673,423.465
+				c-14.427,0-26.122-11.695-26.122-26.122c0-14.427,11.695-26.122,26.122-26.122c14.427,0,26.122,11.695,26.122,26.122
+				C169.796,411.77,158.1,423.465,143.673,423.465z"
+                        />
+                        <path
+                          d="M342.204,350.322c-25.969,0-47.02,21.052-47.02,47.02c0,25.969,21.052,47.02,47.02,47.02s47.02-21.052,47.02-47.02
+				C389.224,371.374,368.173,350.322,342.204,350.322z M342.204,423.465c-14.427,0-26.122-11.695-26.122-26.122
+				c0-14.427,11.695-26.122,26.122-26.122s26.122,11.695,26.122,26.122C368.327,411.77,356.631,423.465,342.204,423.465z"
+                        />
+                        <path
+                          d="M448.261,76.037c-2.176-2.377-5.153-3.865-8.359-4.18L99.788,67.155L90.384,38.42
+				C83.759,19.211,65.771,6.243,45.453,6.028H10.449C4.678,6.028,0,10.706,0,16.477s4.678,10.449,10.449,10.449h35.004
+				c11.361,0.251,21.365,7.546,25.078,18.286l66.351,200.098l-5.224,12.016c-5.827,15.026-4.077,31.938,4.702,45.453
+				c8.695,13.274,23.323,21.466,39.184,21.943h203.233c5.771,0,10.449-4.678,10.449-10.449c0-5.771-4.678-10.449-10.449-10.449
+				H175.543c-8.957-0.224-17.202-4.936-21.943-12.539c-4.688-7.51-5.651-16.762-2.612-25.078l4.18-9.404l219.951-22.988
+				c24.16-2.661,44.034-20.233,49.633-43.886l25.078-105.012C450.96,81.893,450.36,78.492,448.261,76.037z M404.376,185.228
+				c-3.392,15.226-16.319,26.457-31.869,27.69l-217.339,22.465L106.58,88.053l320.261,4.702L404.376,185.228z"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                  <g></g>
+                </svg>
+              </div>
             </div>
             <div className="phone-cart">
               <div className="phone-nav-icon-1">
